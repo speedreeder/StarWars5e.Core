92397a19a8447ffc88b8184137454cfd45f19174
Alexander Reeder <alexgreeder@LIVE.COM>
2019-03-30 22:57:16 -0400

    Cleaned out unnecessary old projects and parsing code.

--- StarWars5e.Models/Interfaces/IMount.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StarWars5e.Models.Interfaces
-{
-    public interface IMount : IEquipment
-    {
-        int CarryingCapacity { get; set; }
-
-        int Speed { get; set; }
-    }
-}
--- StarWars5e.Models/Interfaces/IVehicle.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace StarWars5e.Models.Interfaces
-{
-    public interface IVehicle : IEquipment
-    {
-        int Speed { get; set; }
-    }
-}
