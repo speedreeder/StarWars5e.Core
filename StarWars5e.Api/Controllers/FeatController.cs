a8466edb8fe790ca0be544eb9c08d8b094289765
Alexander Reeder <alexgreeder@LIVE.COM>
2019-04-27 21:47:02 -0400

    Added endpoint for feats.

---
+++ StarWars5e.Api/Controllers/FeatController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StarWars5e.Models;
+using Wolnik.Azure.TableStorage.Repository;
+
+namespace StarWars5e.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeatController : ControllerBase
+    {
+        private readonly ITableStorage _tableStorage;
+
+        public FeatController(ITableStorage tableStorage)
+        {
+            _tableStorage = tableStorage;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Feat>>> Get()
+        {
+            var feats = await _tableStorage.GetAllAsync<Feat>("feats");
+            return Ok(feats);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Feat feat)
+        {
+        }
+
+        [HttpDelete("{name}")]
+        public void Delete(string name)
+        {
+        }
+    }
+}
