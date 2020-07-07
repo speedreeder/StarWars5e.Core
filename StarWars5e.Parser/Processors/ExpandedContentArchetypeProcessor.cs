071981f733e9518ea6d8d6a918c12cc0a7ced451
Alexander Reeder <alexgreeder@LIVE.COM>
2020-07-07 18:21:00 -0400

    Updated content, removed hack for Trakata

--- StarWars5e.Parser/Processors/ExpandedContentArchetypeProcessor.cs
+++ StarWars5e.Parser/Processors/ExpandedContentArchetypeProcessor.cs
@@ -43,9 +43,6 @@ namespace StarWars5e.Parser.Processors
                 return null;
             }).Where(t => t != null).ToList();
 
-            var trakataIndex = archetypeNames.FindIndex(a => a.Equals(Localization.ECFormIXTrakata));
-            archetypeNames[trakataIndex] = Localization.ECFormIXTrakataMangled;
-
             var starWarsClassName = Localization.Berserker;
             foreach (var tableLine in tableLines)
             {
@@ -55,7 +52,6 @@ namespace StarWars5e.Parser.Processors
                 else
                 {
                     var archetypeName = tableLineSplit[1].Trim().RemoveHtmlWhitespace();
-                    if (archetypeName == Localization.ECFormIXTrakata) archetypeName = Localization.ECFormIXTrakataMangled;
                     var archetypeLinesStart = lines.FindIndex(f => f.Contains($"## {archetypeName}"));
 
                     var archetypeLinesEnd = lines.FindIndex(archetypeLinesStart + 1,
