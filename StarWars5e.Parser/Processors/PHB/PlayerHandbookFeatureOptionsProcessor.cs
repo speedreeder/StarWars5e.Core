687b03c169f74384cf92c55b9ed7bd967cb574ea
Jokodin <shurikenbandit@gmail.com>
2021-01-03 23:36:27 -0600

    most feature options parsing

--- StarWars5e.Parser/Processors/PHB/PlayerHandbookFeatureOptionsProcessor.cs
+++ StarWars5e.Parser/Processors/PHB/PlayerHandbookFeatureOptionsProcessor.cs
@@ -11,34 +11,53 @@ namespace StarWars5e.Parser.Processors.PHB
         public override Task<List<FeatureOption>> FindBlocks(List<string> page)
         {
             List<FeatureOption> featureOptions = new List<FeatureOption>();
-            featureOptions.AddRange(parseManeuvers(page, "Fighter"));
-            featureOptions.AddRange(parseManeuvers(page, "Scholar"));
-
+            featureOptions.AddRange(parseOptions(page, "Fighter", "Maneuvers"));
+            featureOptions.AddRange(parseOptions(page, "Scholar", "Maneuvers"));
+            featureOptions.AddRange(parseOptions(page, "Scholar", "Discoveries"));
+            featureOptions.AddRange(parseOptions(page, "Sentinel", "Sentinel Ideals"));
+            featureOptions.AddRange(parseOptions(page, "Guardian", "Guardian Auras"));
+            featureOptions.AddRange(parseOptions(page, "Scout", "Hunter's Prey"));
+            featureOptions.AddRange(parseOptions(page, "Consular", "Force-Empowered Casting"));
 
             return Task.FromResult(featureOptions);
         }
 
-        private List<FeatureOption> parseManeuvers(List<string> lines, string className)
+        private List<FeatureOption> parseOptions(List<string> lines, string className, string optionType)
         {
             var featureOptions = new List<FeatureOption>();
-            var fighterStartIndex = lines.FindIndex(0, f => f.Contains("CHAPTER 3 | CLASSES | " + className.ToUpper()));
-            var maneuverStartIndex = lines.FindIndex(fighterStartIndex, f => f.StartsWith("### Maneuvers"));
-            var maneuverEndIndex = lines.FindIndex(maneuverStartIndex + 1, f => f.StartsWith("### "));
-            var maneuversLines = lines.GetRange(maneuverStartIndex, maneuverEndIndex - (maneuverStartIndex - 1));
-
-            for (var i = 0; i < maneuversLines.Count; i++)
+            var classStartIndex = lines.FindIndex(0, f => f.Contains("CHAPTER 3 | CLASSES | " + className.ToUpper()));
+            var optionsStartIndex = 0;
+            if(optionType.Equals("Sentinel Ideals"))
             {
-                var maneuver = new FeatureOption();
-                if (maneuversLines[i].StartsWith("#### "))
+                // why the fuck is FindLastIndex returning -1?
+                optionsStartIndex = lines.FindIndex(classStartIndex, f => f.StartsWith("### " + optionType));
+                optionsStartIndex = lines.FindIndex(optionsStartIndex+1, f => f.StartsWith("### " + optionType));
+            }
+            else
+            {
+                optionsStartIndex = lines.FindIndex(classStartIndex, f => f.StartsWith("### " + optionType));
+            }
+            var optionsEndIndex = lines.FindIndex(optionsStartIndex + 1, f => f.StartsWith("### "));
+            var optionsLines = lines.GetRange(optionsStartIndex, optionsEndIndex - (optionsStartIndex - 1));
+
+            for (var i = 0; i < optionsLines.Count; i++)
+            {
+                var featureOption = new FeatureOption();
+                if (optionsLines[i].StartsWith("#### "))
                 {
-                    maneuver.Name = maneuversLines[i].Replace("#", "").Trim();
-                    maneuver.Text = maneuversLines[i + 1];
-                    maneuver.Feature = "Class-"+className+"-Maneuvers";
-                    maneuver.RowKey = className+"-Maneuvers-" + maneuver.Name;
-                    maneuver.PartitionKey = "Core";
-                    i++;
-                    featureOptions.Add(maneuver);
-                    Console.WriteLine(maneuver.RowKey);
+                    var nextOptionStartIndex = optionsLines.FindIndex(i+1, f => f.StartsWith("#"))-1;
+                    var numTextLines = nextOptionStartIndex - i;
+                    featureOption.Name = optionsLines[i].Replace("#", "").Trim();
+                    featureOption.Text = "";
+                    
+                    for (var x = 1; x < numTextLines; x++)
+                    {
+                        featureOption.Text += optionsLines[i + x];
+                    }
+                    featureOption.Feature = $"Class-{className}-{optionType}" ;
+                    featureOption.RowKey = $"{className}-{optionType}-{featureOption.Name}";
+                    featureOption.PartitionKey = "Core";
+                    featureOptions.Add(featureOption);
                 }
             }
 
