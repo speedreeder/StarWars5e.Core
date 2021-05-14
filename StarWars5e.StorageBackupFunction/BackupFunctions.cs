24bbaa4dfa0db79e93a6fe69fa323117e91b05a1
Alexander Reeder <alexgreeder@LIVE.COM>
2021-05-13 23:02:16 -0700

    Fixed the table backup function so that it actually backs up tables.

--- StarWars5e.StorageBackupFunction/BackupFunctions.cs
+++ StarWars5e.StorageBackupFunction/BackupFunctions.cs
@@ -1,30 +1,22 @@
 using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using TheByteStuff.AzureTableUtilities;
 
 namespace StarWars5e.StorageBackupFunction
 {
     public static class BackupFunctions
     {
         [FunctionName("BackupTablesAndBlobs")]
-        public static async Task Run([TimerTrigger("0 0 7 * * *", RunOnStartup = true)] TimerInfo myTimer, ILogger log, ExecutionContext context)
+        public static void Run([TimerTrigger("0 0 7 * * *", RunOnStartup = true)] TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
-            var sourceAccountName = Environment.GetEnvironmentVariable("BackupSourceAccountName");
-            var sourceKey = Environment.GetEnvironmentVariable("BackupSourceAccountKey");
+            var backupCs = Environment.GetEnvironmentVariable("BackupStorageConnectionString");
+            var targetCs = Environment.GetEnvironmentVariable("TargetStorageConnectionString");
 
-            var backupAzureStorage = new Luminis.AzureStorageBackup.BackupAzureStorage(sourceAccountName, sourceKey, log, context.FunctionAppDirectory);
+            var backup = new BackupAzureTables(backupCs, targetCs);
 
-            var destinationAccountName = Environment.GetEnvironmentVariable("BackupDestinationAccountName");
-            var destinationKey = Environment.GetEnvironmentVariable("BackupDestinationAccountKey");
-            var destinationContainerName = Environment.GetEnvironmentVariable("BackupDestinationContainer");
-
-            // Backup Tables
-            //await backupAzureStorage.BackupAzureTablesToBlobStorage("table1,table2", destinationAccountName, destinationKey, destinationContainerName, "tables");
-
-            // Backup Blobs
-            await backupAzureStorage.BackupBlobStorage(new List<string>{"characters"}, destinationAccountName, destinationKey, destinationContainerName, "blobs");
+            backup.BackupTableToBlobDirect("characters", "backups", true);
+            backup.BackupTableToBlobDirect("users", "backups", true);
         }
     }
 }
